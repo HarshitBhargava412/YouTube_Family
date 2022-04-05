4d80db10c35c4001019d3c37f39994a4a4086823
Harshit Bhargava <86331803+HarshitBhargava412@users.noreply.github.com>
2022-04-06 02:40:20 +0530

    Code for presetting whatsapp message

---
+++ whatsapp message.py
@@ -0,0 +1,8 @@
+# code for presetting whatsapp message
+
+import pywhatkit as kit
+
+number = "Enter the number with +91"
+# Enter number with country code
+
+kit.sendwhatmsg(number, 'Happy Birthday!!', 24, 10)
