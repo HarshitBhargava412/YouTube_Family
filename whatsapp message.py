559a401d160ed53722b88945420033b60b8b71bc
Harshit Bhargava <86331803+HarshitBhargava412@users.noreply.github.com>
2022-06-09 00:37:38 +0530

    Delete whatsapp message.py

--- whatsapp message.py
+++
@@ -1,8 +0,0 @@
-# code for presetting whatsapp message
-
-import pywhatkit as kit
-
-number = "Enter the number with +91"
-# Enter number with country code
-
-kit.sendwhatmsg(number, 'Happy Birthday!!', 24, 10)
